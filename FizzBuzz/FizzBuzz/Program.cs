cfe82f670b1e38cd0ba2422617fbbcbf8796c802
Codruta Atomei <codruta.atomei@yahoo.com>
2021-03-10 22:42:32 +0200

    First commit

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // nu e necesar sa pui '\n'  ca sa treci la linie noua
+
+            for(int i= 1; i<=100; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("\n FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("\n Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("\n Buzz");
+                }
+                else
+                {
+                    Console.WriteLine("\n " + i);
+                }
+            }
+        }
+    }
+}
