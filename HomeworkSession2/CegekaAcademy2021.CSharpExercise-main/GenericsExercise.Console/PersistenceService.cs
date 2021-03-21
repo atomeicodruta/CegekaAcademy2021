b520c757e2ff4822b7cc94ed8fc5d0fbf8f6617e
Codruta Atomei <codruta.atomei@yahoo.com>
2021-03-21 23:49:23 +0200

    Homework for session 2
    Exercise regarding generic types and exceptions.

---
+++ HomeworkSession2/CegekaAcademy2021.CSharpExercise-main/GenericsExercise.Console/PersistenceService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsExercise.Console
+{
+    // This class acts as an intermediar (service) between the main program and the given Persistence class
+    public class PersistenceService
+    {
+        private readonly Persistence persistence;
+        public PersistenceService()
+        {
+            persistence = new Persistence();
+            
+        }
+
+        public void InsertEntity<TEntity>(TEntity entity) where TEntity : IEntity
+        {
+            // Insert new entity in the "database": student or university (or any other added class implementing IEntity)
+            try
+            {
+                persistence.InsertAsync(entity);
+                System.Console.WriteLine("\tEntry succesfully registered.");
+            }
+            // There are problems with the given id
+            catch (ArgumentException argumentException)
+            { 
+                // Id cannot be empty 
+                if (string.IsNullOrWhiteSpace(entity.Id))
+                {
+                    throw new ArgumentException("\tEntry not registered. Reason: Id cannot be empty.");
+                }
+                //Id cannot contain %
+                else if (entity.Id.Contains("%"))
+                {
+                    throw new ArgumentException("\tEntry not registered. Reason: Id cannot contain %.");
+                }
+                // Id cannot be longer than 10 characters
+                else if (entity.Id.Length > 10 )
+                {
+                    throw new ArgumentException("\tEntry not registered. Reason: Id cannot be longer than 10 characters.");
+                }
+            }
+            // Maximum number of entities of certain type already reached 
+            catch (InvalidOperationException exception)
+            {
+                throw new InvalidOperationException("\tEntry not registered. Reason: Cannot insert more than 3 entities of the same type.");
+            }
+            
+        }
+
+        // IEnumerable<TEntity>         - return type: a "collection" of elements of generic type TEntity
+        // ViewAllEntities              - function name
+        // ViewAllEntities<TEntity>()   - the function is generic and does not take any arguments
+        public IEnumerable<TEntity> ViewAllEntities<TEntity>() where TEntity : IEntity   
+        {
+            return persistence.GetAllAsync<TEntity>().Result;
+        }
+    }
+}
---
+++ HomeworkSession2/CegekaAcademy2021.CSharpExercise-main/GenericsExercise.Console/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericsExercise.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int menuOption = 1;
+            PersistenceService persistenceService = new PersistenceService();
+
+            System.Console.WriteLine("\n\tWelcome!");
+            System.Console.WriteLine("\tChoose the option most suitable for you: \n");
+
+            while ((menuOption >= 1) && (menuOption <= 4))
+            {
+                System.Console.WriteLine("\t1. Insert new student.");
+                System.Console.WriteLine("\t2. Insert new university.");
+                System.Console.WriteLine("\t3. View existing students.");
+                System.Console.WriteLine("\t4. View existing universities.");
+                System.Console.WriteLine("\t5. Exit.");
+
+                // Get user's menu choice
+                menuOption = Convert.ToInt32(System.Console.ReadLine());
+
+                switch (menuOption)
+                {
+                    case 1:
+
+                        // Insert new student 
+                        Student student = new Student();
+
+                        // Get student info
+                        System.Console.WriteLine("\n\tInsert the id of the student: ");
+                        student.Id = System.Console.ReadLine();
+
+                        System.Console.WriteLine("\n\tInsert the firstname of the student: ");
+                        student.FisrtName = System.Console.ReadLine();
+
+                        System.Console.WriteLine("\n\tInsert the lastname of the student: ");
+                        student.LastName = System.Console.ReadLine();
+
+                        // Call function to insert the new student 
+                        try
+                        {
+                            persistenceService.InsertEntity(student);
+                        }
+                        catch (ArgumentException argumentException)
+                        {
+                            System.Console.WriteLine(argumentException.Message);
+                        }
+                        catch (InvalidOperationException exception)
+                        {
+                            System.Console.WriteLine("\tEntry not registered. Reason: Cannot register more than 3 students.");
+                        }
+
+                        break;
+                    case 2:
+                        // Insert new university
+                        University university = new University();
+
+                        // Get university info
+                        System.Console.WriteLine("\n\tInsert the id of the university: ");
+                        university.Id = System.Console.ReadLine();
+
+                        System.Console.WriteLine("\n\tInsert the name of the university: ");
+                        university.Name = System.Console.ReadLine();
+
+                        System.Console.WriteLine("\n\tInsert the address of the university: ");
+                        university.Address = System.Console.ReadLine();
+
+                        // Call function to insert the new university
+                        try
+                        {
+                            persistenceService.InsertEntity(university);  
+                        }
+                        catch(ArgumentException argumentException)
+                        {
+                            System.Console.WriteLine(argumentException.Message);
+                        }
+                        catch (InvalidOperationException exception)
+                        {
+                            System.Console.WriteLine("\tEntry not registered. Reason: Cannot register more than 3 universities.");
+                        }
+
+                        break;
+                    case 3:
+                        // View existing students
+                        try
+                        {
+                            IEnumerable<Student> students = persistenceService.ViewAllEntities<Student>();
+                            foreach (Student existingStudent in students)
+                            {
+                                System.Console.WriteLine("\t\t Student ID: " + existingStudent.Id +
+                                    "\n\t\t Student First Name: " + existingStudent.FisrtName +
+                                    "\n\t\t Student Last Name: " + existingStudent.LastName + "\n");
+                            }
+                        }
+                        catch (InvalidOperationException exception)
+                        {
+                            System.Console.WriteLine("\n\tOops...looks like there are no students registered yet.");
+                        }
+                        
+                        break;
+
+                    case 4:
+                        // View existing universities
+                        try
+                        {
+                            IEnumerable<University> universities = persistenceService.ViewAllEntities<University>();
+                            foreach (University existingUniversity in universities)
+                            {
+                                System.Console.WriteLine("\t\t University ID: " + existingUniversity.Id +
+                                    "\n\t\t University Name: " + existingUniversity.Name +
+                                    "\n\t\t University Address: " + existingUniversity.Address + "\n");
+                            }
+                        }
+                        catch (InvalidOperationException exception)
+                        {
+                            System.Console.WriteLine("\n\tOops...looks like there are no universities registered yet.");
+                        }
+
+                        break;
+
+                    case 5:
+                        // Exit program
+                        System.Console.WriteLine("\n\tGoodbye for now...\n");
+                        break;
+                    default:
+                        System.Console.WriteLine("\n\tOops...looks like you entered an undefined command.");
+                        break;
+                }
+
+                if (menuOption != 5)
+                {
+                    System.Console.WriteLine("\n\tGo back to menu?");
+                    System.Console.WriteLine("\t\tYes - 1\n\t\tNo - 0");
+
+                    menuOption = Convert.ToInt32(System.Console.ReadLine());
+                    if (menuOption == 0)
+                    {
+                        System.Console.WriteLine("\n\tGoodbye for now...\n");
+                    }
+                }
+            }
+        }
+    }
+}
